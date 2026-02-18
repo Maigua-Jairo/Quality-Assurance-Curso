5d91367097f7edbbc9e7a28a382d032aaace352b
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-02-17 19:20:34 -0500

    labeled 3 change 5

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -17,7 +17,7 @@ suite('Unit Tests', function () {
     // #3
     test('#isOk, #isNotOk', function () {
       assert.isNotOk(null, 'null is falsey');
-      assert.isOk("I'm truthy", 'A string is truthy');
+      assert.isNotOk("I'm truthy", 'A string is truthy');
       assert.isNotOk(true, 'true is truthy');
     });
     // #4

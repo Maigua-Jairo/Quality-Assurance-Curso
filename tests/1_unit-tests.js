c9a3947f94d7fdd549a740dd5decbde5a949a8e3
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-02-25 20:20:16 -0500

    Change 2 under label #8

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -64,7 +64,7 @@ suite('Unit Tests', function () {
       assert.isAtMost('hello'.length, 5);
       assert.isAbove(1, 0);
       assert.isAbove(Math.PI, 3);
-      assert.isAtMost(1 - Math.random(), 1);
+      assert.isAbove(1 - Math.random(), 1);
     });
     // #9
     test('#isBelow, #isAtLeast', function () {

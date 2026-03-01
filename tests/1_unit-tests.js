379dce46b5654c08b929efdc0c6b0152eb03458b
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-02-28 21:51:39 -0500

    change 2 under label #16

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -143,9 +143,9 @@ suite('Unit Tests', function () {
   suite('Objects', function () {
     // #16
     test('#property, #notProperty', function () {
-      assert.property(myCar, 'wings', "Cars don't have wings");
-      assert.notProperty(airlinePlane, 'engines', 'Planes have engines');
-      assert.notProperty(myCar, 'wheels', 'Cars have wheels');
+      assert.notProperty(myCar, 'wings', "Cars don't have wings");
+      assert.property(airlinePlane, 'engines', 'Planes have engines');
+      assert.property(myCar, 'wheels', 'Cars have wheels');
     });
     // #17
     test('#typeOf, #notTypeOf', function () {

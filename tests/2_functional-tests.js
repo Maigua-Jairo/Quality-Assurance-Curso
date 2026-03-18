9f595cba210cd3f70729bee37a3dff5fed360548
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-03-18 10:44:52 -0500

    Test2 #4 change 3

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -58,7 +58,7 @@ suite('Functional Tests', function () {
           done();
         });
     });
-    // #4
+// #4
     test('Send {surname: "da Verrazzano"}', function (done) {
       chai
        .request(server)
@@ -68,21 +68,13 @@ suite('Functional Tests', function () {
        .end(function (err, res) {
          assert.equal(res.status, 200, 'response status should be 200');
          assert.equal(res.type, 'application/json', 'Response should be json');
-         assert.equal(
-           res.body.name,
-           'Giovanni',
-           'res.body.name should be "Giovanni"'
-         );
-         assert.equal(
-          res.body.surname,
-          'da Verrazzano',
-          'res.body.surname should be "da Verrazzano"'
-         );
-
-      done();
-    });
-  });
-});
+         assert.equal(res.body.name, 'Giovanni', 'res.body.name should be "Giovanni"');
+         assert.equal(res.body.surname, 'da Verrazzano', 'res.body.surname should be "da Verrazzano"');
+         done();
+       });   
+    });     
+  });      
+});         
 
 const Browser = require('zombie');
 
